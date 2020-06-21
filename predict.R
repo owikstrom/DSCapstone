87be91fc9adef6e53ec2cfbd5a0f10439e17d3ff
Olov Wikstrom <olov.wikstrom@gmail.com>
2020-06-21 19:57:43 +0200

    bugfixes for uppercase

--- predict.R
+++ predict.R
@@ -1,19 +1,23 @@
 predictWord <- function(phrase, db){
     
-    query <- as.character(tokens(phrase,
+    query <- tolower(as.character(tokens(phrase,
                                  remove_punct =TRUE, 
                                  remove_symbols=TRUE, 
-                                 remove_url=TRUE)) 
-  
-    for (i in length(db):1) {
-      subquery <- tail(query,i) %>% paste(collapse="_")
+                                 remove_url=TRUE))) 
 
+    for (i in length(db):2) {
+      subquery <- tail(query,i) %>% paste(collapse="_")
+      print(subquery)
       result <- db[[i]] %>% filter(input == subquery) %>% arrange(desc(prob))
-      
-      if(dim(result)[1] >0) break
-             
-    }   
-    result[1,3]
+
+      if(dim(result)[1] >0){
+        break
+      } else if(i==2) {
+        result <- db[[1]] %>% filter(input == "") %>% arrange(desc(prob))
+        result <- result[sample(nrow(result), 1, replace=TRUE, prob = result$prob),]
+      }
+    }  
+    result[1,2]
     
 # Maybe recalculate for unobserved n-grams
 # sample(resultn$type, 1, F, resultsn$prob)       
