eeee6a8ae110e1e90792264e0708a7b482117d17
Olov Wikstrom <olov.wikstrom@gmail.com>
2020-06-21 22:33:12 +0200

    Implemented a alternate version of the prediction method.

--- predict.R
+++ predict.R
@@ -1,13 +1,14 @@
 predictWord <- function(phrase, db){
-    
+    require(quanteda)
+    require(dplyr)
+  
     query <- tolower(as.character(tokens(phrase,
                                  remove_punct =TRUE, 
                                  remove_symbols=TRUE, 
                                  remove_url=TRUE))) 
 
     for (i in length(db):2) {
-      subquery <- tail(query,i) %>% paste(collapse="_")
-      print(subquery)
+      subquery <- tail(query,i-1) %>% paste(collapse="_")
       result <- db[[i]] %>% filter(input == subquery) %>% arrange(desc(prob))
 
       if(dim(result)[1] >0){
@@ -16,14 +17,37 @@ predictWord <- function(phrase, db){
         result <- db[[1]] %>% filter(input == "") %>% arrange(desc(prob))
         result <- result[sample(nrow(result), 1, replace=TRUE, prob = result$prob),]
       }
-    }  
+    }
     result[1,2]
-    
-# Maybe recalculate for unobserved n-grams
-# sample(resultn$type, 1, F, resultsn$prob)       
-        
-        
 } 
+
+
+predictWordDetailed <- function(phrase, db){
+
+  require(quanteda)
+  require(dplyr)
+  
+  query <- tolower(as.character(tokens(phrase,
+                                       remove_punct =TRUE, 
+                                       remove_symbols=TRUE, 
+                                       remove_url=TRUE))) 
+
+    for (i in length(db):2) {
+    subquery <- tail(query,i-1) %>% paste(collapse="_")
+    result <- db[[i]] %>% filter(input == subquery) %>% arrange(desc(prob))
+    
+    if(dim(result)[1] >0){
+      break
+    } else if(i==2) {
+      result <- db[[1]] %>% filter(input == "") %>% arrange(desc(prob))
+      result <- result[sample(nrow(result), 1, replace=TRUE, prob = result$prob),]
+    }
+  }
+  result
+} 
+
+
+
 
 queryWord <- function(phrase, answers, db){
   
