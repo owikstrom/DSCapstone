bd03ddd7ca979b5a21182a1a93361d343e21f302
Olov Wikstrom <olov.wikstrom@gmail.com>
2020-06-21 16:47:56 +0200

    1st working update. Functions for lookup and the quizes

--- predict.R
+++ predict.R
@@ -1,22 +1,52 @@
-predictWord <- function(phrase, ngramdb){
-
-library(tidyverse)
+predictWord <- function(phrase, db){
+    
+    query <- as.character(tokens(phrase,
+                                 remove_punct =TRUE, 
+                                 remove_symbols=TRUE, 
+                                 remove_url=TRUE)) 
+  
+    for (i in length(db):1) {
+      
+    
+ 
+      subquery <- tail(query,i) %>% paste(collapse="_")
+      
+      print(subquery)
+      
          
-        
-        
-for (i in 5:1) {
-
-snippet <- phrase %>% word(-4:1,-1)
-        
-        
-resultn <- ngramdb[[i]][snippet]
-
-if(len(result >0)) break
-        
-}              
+      result <- db[[i]] %>% filter(input == subquery) %>% arrange(desc(prob))
+      print(head(result,50))
+     
+      # if(dim(result)[1] >0) break
+             
+    }   
+    result
     
 # Maybe recalculate for unobserved n-grams
 # sample(resultn$type, 1, F, resultsn$prob)       
         
         
+} 
+
+queryWord <- function(phrase, answers, db){
+  
+  query <- as.character(tokens(phrase)) 
+  
+  for (i in length(db):1) {
+    
+    
+    
+    subquery <- tail(query,i) %>% paste(collapse="_")
+   
+    result <- db[[i]] %>% filter(input == subquery) %>% arrange(desc(prob))
+    result <- result[which(result$prediction %in% unlist(answers)),]
+ 
+  if(dim(result)[1] >0) break
+     
+  }   
+  result[1,3]
+  # Maybe recalculate for unobserved n-grams
+  # sample(resultn$type, 1, F, resultsn$prob)       
+  
+  
 } 
